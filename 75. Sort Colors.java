7d715762db859570f07547619b1316f6b5437431
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-09-22 10:44:13 +0530

    Update 75. Sort Colors.java

--- 75. Sort Colors.java
+++ 75. Sort Colors.java
@@ -1,1 +1,32 @@
 
+class Solution {
+    public void sortColors(int[] nums) {
+        int one = 0, two = 0, zero = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1)
+                one++;
+            if (nums[i] == 2)
+                two++;
+            if (nums[i] == 0)
+                zero++;
+        }
+        int count = 0;
+        while (count < nums.length) {
+            while (zero > 0 && count < nums.length) {
+                nums[count] = 0;
+                count++;
+                zero--;
+            }
+            while (one > 0 && count < nums.length) {
+                nums[count] = 1;
+                count++;
+                one--;
+            }
+            while (two > 0 && count < nums.length) {
+                nums[count] = 2;
+                count++;
+                two--;
+            }
+        }
+    }
+}
