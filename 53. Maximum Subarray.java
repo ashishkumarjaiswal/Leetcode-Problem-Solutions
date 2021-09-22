314dc2ba5de9e7858182d91f3389b7632325aed0
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-09-22 13:57:40 +0530

    Create 53. Maximum Subarray.java
    53. Maximum Subarray

---
+++ 53. Maximum Subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int csum = nums[0];
+        int osum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (csum > 0)
+                csum = csum + nums[i];
+            else
+                csum = nums[i];
+            if (osum < csum)
+                osum = csum;
+        }
+        return osum;
+    }
+}
