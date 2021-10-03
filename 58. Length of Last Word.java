0bdc23a863de44260202a0b92af3674977a4a4f4
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-10-03 17:09:56 +0530

    Update 58. Length of Last Word.java
    58. Length of Last Word
    
    Leetcode problem Solution in Java

--- 58. Length of Last Word.java
+++ 58. Length of Last Word.java
@@ -1,1 +1,22 @@
-
+class Solution {
+    public int lengthOfLastWord(String s) {
+        boolean ashish = true;
+        int i = s.length() - 1;
+        int count = 0;
+        while (i >= 0) {
+            if (s.charAt(i) == ' ' && ashish) {
+                i--;
+                continue;
+            } else {
+                ashish = false;
+            }
+            if (s.charAt(i) == ' ') {
+                break;
+            } else {
+                count++;
+            }
+            i--;
+        }
+        return count;
+    }
+}
