eb3ef7005648646aba54ef9b709fc425c744c921
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-10-12 11:07:08 +0530

    Update 189. Rotate Array.java

--- 189. Rotate Array.java
+++ 189. Rotate Array.java
@@ -1,3 +1,4 @@
+//Method 1
 class Solution {
     public void rotate(int[] nums, int k) {
         int count = 0, len = nums.length;
@@ -8,6 +9,29 @@ class Solution {
         for (int i = 0; i < len; i++) {
             nums[(x + i) % len] = ans[count];
             count++;
+        }
+    }
+}
+
+
+//Method 2
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if (k == 0)
+            return;
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    void reverse(int nums[], int s, int e) {
+        while (s < e) {
+            int temp = nums[s];
+            nums[s] = nums[e];
+            nums[e] = temp;
+            s++;
+            e--;
         }
     }
 }
