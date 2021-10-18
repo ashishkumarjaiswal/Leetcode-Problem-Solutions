a1b232c3ca311439f22a0d8b125f3d36d8ab0269
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-10-18 10:00:02 +0530

    Create 125. Valid Palindrome.java
    125. Valid Palindrome
    
    Leetcode Problem Solution in Java

---
+++ 125. Valid Palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i <= j) {
+            if (!Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            } else if (!Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            } else {
+                if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+}
