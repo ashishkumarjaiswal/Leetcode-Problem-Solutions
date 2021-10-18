8b220f6853e5b7267d1347a3772665ab97fda1f4
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-10-18 10:45:25 +0530

    Update 1. Two Sum.java

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -1,3 +1,21 @@
+//O(n)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                ans[1] = i;
+                ans[0] = map.get(target - nums[i]);
+                return ans;
+            }
+            map.put(nums[i], i);
+        }
+        return ans;
+    }
+}
+
+//O(n^2)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 int arr[] = new int[2];
