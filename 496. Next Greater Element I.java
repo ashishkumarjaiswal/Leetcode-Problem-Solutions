73d1ab7da3fe5f681224ccbff5adb8f58950e243
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-10-19 09:13:04 +0530

    Update 496. Next Greater Element I.java

--- 496. Next Greater Element I.java
+++ 496. Next Greater Element I.java
@@ -1,4 +1,4 @@
-// By Looping
+// By brute force
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -19,5 +19,28 @@ class Solution {
             arr[i] = max;
         }
         return arr;
+    }
+}
+
+//By Using Stack
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+        int[] arr = new int[nums2.length];
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!st.isEmpty() && st.peek() < nums2[i])
+                st.pop();
+            arr[i] = st.isEmpty() ? -1 : st.peek();
+            st.add(nums2[i]);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            map.put(nums2[i], arr[i]);
+        }
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = map.get(nums1[i]);
+        }
+        return ans;
     }
 }
