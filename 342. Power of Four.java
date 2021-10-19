6e0726b406bbb445f207a8ba6cf373fc64cc66ba
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-10-19 10:23:15 +0530

    Create 342. Power of Four.java
    342. Power of Four
    
    Leetcode Problem Solution in java

---
+++ 342. Power of Four.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0)
+            return false;
+        while (n > 1) {
+            if (n % 4 == 0)
+                n = n / 4;
+            else
+                return false;
+        }
+        return true;
+    }
+}
