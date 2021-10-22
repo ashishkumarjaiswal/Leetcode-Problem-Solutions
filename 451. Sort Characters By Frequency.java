30f1d1afbc6c7d0510f6357d5bb97c36d4728657
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-10-22 10:10:43 +0530

    Update 451. Sort Characters By Frequency.java

--- 451. Sort Characters By Frequency.java
+++ 451. Sort Characters By Frequency.java
@@ -1,3 +1,32 @@
+//Optimize Way
+class Solution {
+    public String frequencySort(String s) {
+        int freq[][] = new int[123][2];
+        for (int i = 0; i < 123; i++)
+            freq[i][0] = i;
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[(s.charAt(i))][1]++;
+        }
+
+        Arrays.sort(freq, (a, b) -> b[1] - a[1]);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i][1] == 0)
+                break;
+            for (int j = 0; j < freq[i][1]; j++)
+                sb.append((char) freq[i][0]);
+
+        }
+        return sb.toString();
+    }
+}
+
+
+
+
+//By HashMap
 class Solution {
     public String frequencySort(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
