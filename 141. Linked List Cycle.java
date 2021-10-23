bef350e4ea2640680ae06d42eaa9bcb2a268e1a3
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-10-23 08:42:52 +0530

    Update 141. Linked List Cycle.java

--- 141. Linked List Cycle.java
+++ 141. Linked List Cycle.java
@@ -12,3 +12,17 @@ public class Solution {
 }
 
 //Using two pointer
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null)
+            return false;
+        ListNode slow = head, fast = head;
+        while (slow.next != null && fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+}
