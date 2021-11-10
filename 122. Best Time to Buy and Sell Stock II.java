e4fc655fb6b34274f0aa88600c08f2102c6105b3
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-11-10 19:52:53 +0530

    Update 122. Best Time to Buy and Sell Stock II.java

--- 122. Best Time to Buy and Sell Stock II.java
+++ 122. Best Time to Buy and Sell Stock II.java
@@ -20,3 +20,21 @@ class Solution {
         return total;
     }
 }
+//Another one
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 1)
+            return 0;
+        int total = 0;
+        int buy = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < prices[i - 1]) {
+                total = total + prices[i - 1] - prices[buy];
+                buy = i;
+            }
+        }
+        if (prices[prices.length - 1] > prices[prices.length - 2])
+            total = total + prices[prices.length - 1] - prices[buy];
+        return total;
+    }
+}
