8e41f1ac5a493d40569e8ff84426345b809eb510
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-11-11 10:39:08 +0530

    Update 1137. N-th Tribonacci Number.java

--- 1137. N-th Tribonacci Number.java
+++ 1137. N-th Tribonacci Number.java
@@ -16,3 +16,20 @@ class Solution {
         return arr[n];
     }
 }
+//Another one
+class Solution {
+    public int tribonacci(int n) {
+        int arr[] = new int[n + 1];
+        return tib(n, arr);
+    }
+
+    int tib(int n, int[] arr) {
+        if (n == 0)
+            return arr[0] = 0;
+        if (n == 1 ||n == 2)
+            return arr[1] = 1;
+        if (arr[n] != 0)
+            return arr[n];
+        return arr[n] = tib(n - 1, arr) + tib(n - 2, arr) + tib(n - 3, arr);
+    }
+}
