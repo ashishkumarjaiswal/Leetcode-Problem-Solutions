a33dd16b17979bb50fe2319d5674b810eb7362d7
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-11-17 19:01:57 +0530

    Update 62. Unique Paths.java

--- 62. Unique Paths.java
+++ 62. Unique Paths.java
@@ -26,3 +26,25 @@ class Solution {
         return x;
     }
 }
+//Another one
+class Solution {
+
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int[m][n];
+        int total = fun(m, n, 0, 0, dp);
+        return total;
+    }
+
+    int fun(int m, int n, int i, int j, int[][] dp) {
+        if (i == m - 1 && j == n - 1)
+            return 1;
+        if (i >= m || j >= n) {
+            return 0;
+        }
+        if (dp[i][j] != 0) {
+            return dp[i][j];
+        }
+        dp[i][j] = fun(m, n, i + 1, j, dp) + fun(m, n, i, j + 1, dp);
+        return dp[i][j];
+    }
+}
