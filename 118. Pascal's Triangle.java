76858e9fae7a063bb40e1f7a670af9403537ec2d
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-11-22 09:02:36 +0530

    Update 118. Pascal's Triangle.java

--- 118. Pascal's Triangle.java
+++ 118. Pascal's Triangle.java
@@ -25,3 +25,29 @@ class Solution {
         return ashish;
     }
 }
+//Another One
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        temp.add(1);
+        ans.add(temp);
+        if (numRows == 1)
+            return ans;
+        temp=new ArrayList<>();
+        temp.add(1);
+        temp.add(1);
+        ans.add(temp);
+        for (int i = 2; i < numRows; i++) {
+            List<Integer> list = new ArrayList<>();
+            list.add(1);
+            for (int j = 1; j < i; j++) {
+                list.add(temp.get(j - 1) + temp.get(j));
+            }
+            list.add(1);
+            temp = new ArrayList<>(list);
+            ans.add(temp);
+        }
+        return ans;
+    }
+}
