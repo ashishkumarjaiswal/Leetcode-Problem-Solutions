654c94633587a5dee20dca0e3d0750e825fabbc6
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-11-25 08:05:54 +0530

    Update 63. Unique Paths II.java

--- 63. Unique Paths II.java
+++ 63. Unique Paths II.java
@@ -27,3 +27,25 @@ class Solution {
         return x;
     }
 }
+//Another one
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        Integer[][] dp = new Integer[m][n];
+        return fun(m, n, dp, 0, 0, obstacleGrid);
+    }
+
+    int fun(int m, int n, Integer[][] dp, int i, int j, int[][] obstacleGrid) {
+        if (i >= m || j >= n || obstacleGrid[i][j] == 1) {
+            return 0;
+        }
+        if (i == m - 1 && j == n - 1) {
+            return 1;
+        }
+        if (dp[i][j] != null) {
+            return dp[i][j];
+        }
+        dp[i][j] = fun(m, n, dp, i + 1, j, obstacleGrid) + fun(m, n, dp, i, j + 1, obstacleGrid);
+        return dp[i][j];
+    }
+}
