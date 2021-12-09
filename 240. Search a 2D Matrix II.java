2f48772ab595e415a0add1110334f6ac649d747c
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-12-09 18:09:46 +0530

    Create 240. Search a 2D Matrix II.java
    240. Search a 2D Matrix II
    
    LeetCode Problem Solution in Java

---
+++ 240. Search a 2D Matrix II.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0, col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target)
+                return true;
+            if (matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+
+        }
+        return false;
+    }
+}
