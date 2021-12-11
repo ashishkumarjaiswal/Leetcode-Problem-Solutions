99689e129874e12acc028724935d2120706f96a5
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-12-11 11:09:11 +0530

    Update 78. Subsets.java

--- 78. Subsets.java
+++ 78. Subsets.java
@@ -13,3 +13,25 @@ class Solution {
         return ret;
     }
 }
+//By Recurssion
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        fun(nums, 0, new ArrayList<>());
+        return result;
+    }
+
+    void fun(int[] nums, int ind, List<Integer> list) {
+        if (ind == nums.length) {
+            result.add(list);
+            return;
+        }
+        list.add(nums[ind]);
+        //is come
+        fun(nums, ind + 1, new ArrayList<>(list));
+        list.remove(list.size() - 1);
+        //not come
+        fun(nums, ind + 1, new ArrayList<>(list));
+    }
+}
