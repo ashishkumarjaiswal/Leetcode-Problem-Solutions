eaab1a1ef7f36095b1684ab6760d59bc5c62db02
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-12-13 06:52:15 +0530

    Update 46. Permutations.java

--- 46. Permutations.java
+++ 46. Permutations.java
@@ -25,3 +25,29 @@ class Solution {
 
     }
 }
+//Another one
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        boolean[] visited = new boolean[nums.length];
+        fun(nums, visited, new ArrayList<>());
+        return ans;
+    }
+
+    List<List<Integer>> ans = new ArrayList<>();
+
+    void fun(int[] nums, boolean[] visited, List<Integer> list) {
+        if (list.size() == nums.length) {
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                list.add(nums[i]);
+                fun(nums, visited, list);
+                visited[i] = false;
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+}
