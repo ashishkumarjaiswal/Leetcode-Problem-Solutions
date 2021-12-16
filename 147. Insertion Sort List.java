a4ac14e92017c93a2c5e607c1ef666ef59813ca6
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-12-16 17:43:12 +0530

    Update 147. Insertion Sort List.java
    LeetCode Problem Solution in java

--- 147. Insertion Sort List.java
+++ 147. Insertion Sort List.java
@@ -1,1 +1,30 @@
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        // ListNode ans = new ListNode();
+        ListNode result = new ListNode();
+        if (head != null)
+            result.val = head.val;
+        head = head.next;
+        while (head != null) {
+            int temp = head.val;
+            if (temp < result.val) {
+                ListNode a = new ListNode(temp);
+                a.next = result;
+                result = a;
+                head = head.next;
+                continue;
+            }
+            ListNode x = result;
+            while (x.next != null && x.next.val < temp) {
+                x = x.next;
+            }
+            ListNode next = x.next;
+            x.next = new ListNode(temp);
+            x = x.next;
+            x.next = next;
 
+            head = head.next;
+        }
+        return result;
+    }
+}
