a6d392f5c79c4f4ba0ed9864b2cdff2672c5ec06
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-12-24 06:58:58 -0500

    Update 1184. Distance Between Bus Stops.java
    LeetCode Problem Solution in Java

--- 1184. Distance Between Bus Stops.java
+++ 1184. Distance Between Bus Stops.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public int distanceBetweenBusStops(int[] distance, int start, int destination) {
+        int sum = 0;
+        int n = distance.length;
+        for (int i = start; i % n != destination; i++) {
+            sum = sum + distance[i % n];
+        }
+        int total = 0;
+        for (int i = 0; i < distance.length; i++) {
+            total += distance[i];
+        }
+        return Math.min(sum, total - sum);
+    }
+}
