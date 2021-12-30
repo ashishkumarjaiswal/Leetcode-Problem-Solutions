04cdf4df3c52bc9ea44b270847d835062ba5eabb
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-12-30 13:11:55 +0530

    Update 148. Sort List.java

--- 148. Sort List.java
+++ 148. Sort List.java
@@ -16,3 +16,55 @@ class Solution {
         return result.next;
     }
 }
+
+//Using merge sort
+class Solution {
+    public ListNode sortList(ListNode head) {
+        return sort(head);
+    }
+
+    ListNode sort(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode middle = middle(head);
+        ListNode second = sort(middle.next);
+        middle.next = null;
+        ListNode first = sort(head);
+        return merge(first, second);
+    }
+
+    ListNode merge(ListNode one, ListNode two) {
+        ListNode ans = new ListNode(56);
+        ListNode result = ans;
+        while (one != null && two != null) {
+            if (one.val > two.val) {
+                ans.next = new ListNode(two.val);
+                two = two.next;
+            } else {
+                ans.next = new ListNode(one.val);
+                one = one.next;
+            }
+            ans = ans.next;
+        }
+        if (one != null) {
+            ans.next = one;
+        }
+        if (two != null) {
+            ans.next = two;
+        }
+        return result.next;
+    }
+
+    ListNode middle(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
