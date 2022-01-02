aea86863217695627ef74e055943a8aae70373bf
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2022-01-02 11:43:20 +0530

    Update 494. Target Sum.java

--- 494. Target Sum.java
+++ 494. Target Sum.java
@@ -1,3 +1,4 @@
+//Without dynamic programming only backtracking
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         fun(nums, target, 0);
@@ -14,5 +15,27 @@ class Solution {
         }
         fun(nums, target + nums[start], start + 1);
         fun(nums, target - nums[start], start + 1);
+    }
+}
+//using both dynamic programming and backtracking
+class Solution {
+    HashMap<String, Integer> map = new HashMap<>();
+
+    public int findTargetSumWays(int[] nums, int target) {
+        return fun(nums, target, 0, 0);
+    }
+
+    int fun(int[] nums, int target, int start, int count) {
+        if (map.containsKey(target + " " + start))
+            return map.get(target + " " + start);
+        if (start == nums.length) {
+            if (target == 0)
+                count++;
+            return count;
+        }
+        int x = fun(nums, target + nums[start], start + 1, 0);
+        int y = fun(nums, target - nums[start], start + 1, 0);
+        map.put(target + " " + start, x + y);
+        return x + y;
     }
 }
