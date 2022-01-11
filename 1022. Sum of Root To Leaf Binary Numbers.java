269a67d22acb5c9d889b1871fe7cffe06db5156e
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2022-01-11 07:21:01 +0530

    Update 1022. Sum of Root To Leaf Binary Numbers.java

--- 1022. Sum of Root To Leaf Binary Numbers.java
+++ 1022. Sum of Root To Leaf Binary Numbers.java
@@ -16,3 +16,31 @@ class Solution {
         fun(root.right, curr);
     }
 }
+
+
+//Another approach using string list
+class Solution {
+    public int sumRootToLeaf(TreeNode root) {
+        fun(root, "");
+        int total = 0;
+        for (int i = 0; i < ans.size(); i++) {
+            total = total + Integer.parseInt(ans.get(i), 2);
+        }
+        return total;
+    }
+
+    List<String> ans = new ArrayList<>();
+
+    void fun(TreeNode root, String str) {
+        if (root == null)
+            return;
+        if (root.left == null && root.right == null) {
+            ans.add(str + root.val);
+            return;
+        }
+        str = str + root.val;
+        fun(root.left, str);
+        fun(root.right, str);
+        str = str.substring(0, str.length() - 1);
+    }
+}
