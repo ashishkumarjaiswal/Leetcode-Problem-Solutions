fe897837f64b3b27ff4e46516ad57a070c23ce38
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2022-01-22 16:11:31 +0530

    Update 875. Koko Eating Bananas.java

--- 875. Koko Eating Bananas.java
+++ 875. Koko Eating Bananas.java
@@ -1,1 +1,28 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+        int s = 1, e = piles[piles.length - 1];
+        while (s <= e) {
+            int mid = (s + e) / 2;
+            if (fun(piles, mid) > h) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+        return s;
+    }
 
+    int fun(int[] piles, int speed) {
+        int count = 0;
+        for (int i = 0; i < piles.length; i++) {
+            if (piles[i] < speed)
+                count++;
+            else if (piles[i] % speed == 0)
+                count = count + piles[i] / speed;
+            else
+                count = count + (piles[i] / speed) + 1;
+        }
+        return count;
+    }
+}
