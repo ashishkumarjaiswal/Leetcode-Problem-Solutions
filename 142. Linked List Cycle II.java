7b1003477a2c8561174805efc8fd657daa7744ea
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2022-01-22 15:56:32 +0530

    Update 142. Linked List Cycle II.java

--- 142. Linked List Cycle II.java
+++ 142. Linked List Cycle II.java
@@ -1,1 +1,12 @@
-
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (!set.add(head)) {
+                return head;
+            }
+            head = head.next;
+        }
+        return null;
+    }
+}
