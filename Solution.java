c18588a1f5899f30cb67e39d29cb32cb35c67ee1
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2022-01-24 10:27:15 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.*;
+
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        List<Integer> pos = new ArrayList<>();
+        List<Integer> neg = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0)
+                pos.add(nums[i]);
+            else
+                neg.add(nums[i]);
+        }
+        int x = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                nums[i] = pos.get(x);
+            } else {
+                nums[i] = neg.get(x);
+                x++;
+            }
+        }
+        return nums;
+    }
+}
