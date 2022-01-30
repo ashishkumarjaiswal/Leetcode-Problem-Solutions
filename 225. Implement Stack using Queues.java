87e9e29fd484a75206de847f12421cac20c5cb30
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2022-01-30 16:55:39 +0530

    Update 225. Implement Stack using Queues.java

--- 225. Implement Stack using Queues.java
+++ 225. Implement Stack using Queues.java
@@ -8,6 +8,36 @@ class MyStack {
 
     public void push(int x) {
         q.add(x);
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.add(q.poll());
+        }
+    }
+
+    public int pop() {
+        return q.poll();
+    }
+
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
+
+//Another one
+class MyStack {
+
+    Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q.add(x);
         System.out.println(q.toString());
     }
 
