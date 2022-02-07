d58185a31c019e0483705ece68592e2cccc7e99f
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2022-02-07 18:52:26 +0530

    Update 264. Ugly Number II.java
    LeetCode Problem Solution in java

--- 264. Ugly Number II.java
+++ 264. Ugly Number II.java
@@ -1,1 +1,32 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
 
+        int p2 = 1;
+        int p3 = 1;
+        int p5 = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int m2 = 2 * dp[p2];
+            int m3 = 3 * dp[p3];
+            int m5 = 5 * dp[p5];
+
+            int min = Math.min(Math.min(m2, m3), m5);
+
+            dp[i] = min;
+
+            if (min == m2){
+                p2++;
+            }
+            if (min == m3){
+                p3++;
+            }
+            if (min == m5){
+                p5++;
+            }
+        }
+
+        return dp[n];
+    }
+}
