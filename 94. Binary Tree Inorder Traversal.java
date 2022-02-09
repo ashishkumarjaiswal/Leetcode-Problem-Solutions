f84cc72af16caac54957b12d9b443a5d732db56e
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2022-02-09 18:05:43 +0530

    Update 94. Binary Tree Inorder Traversal.java

--- 94. Binary Tree Inorder Traversal.java
+++ 94. Binary Tree Inorder Traversal.java
@@ -13,3 +13,37 @@ class Solution {
         in(ashish, root.right);
     }
 }
+
+// Using Morris Traversal
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        TreeNode curr = root;
+        while (curr != null) {
+            TreeNode leftwala = curr.left;
+            if (leftwala == null) {
+                ans.add(curr.val);
+                curr = curr.right;
+            } else {
+                TreeNode rightmost = rightMostNode(leftwala, curr);
+                if (rightmost.right == null) {
+                    rightmost.right = curr;
+                    curr = curr.left;
+                } else {
+                    rightmost.right = null;
+                    ans.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+        }
+        return ans;
+    }
+
+    TreeNode rightMostNode(TreeNode left, TreeNode curr) {
+        while (left.right != null && left.right != curr) {
+            left = left.right;
+        }
+        return left;
+    }
+}
