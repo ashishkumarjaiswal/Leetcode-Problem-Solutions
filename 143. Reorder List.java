c6ca3a536d41dccce2320319f681719ce691e77c
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2022-02-10 09:56:20 +0530

    Update 143. Reorder List.java

--- 143. Reorder List.java
+++ 143. Reorder List.java
@@ -1,1 +1,44 @@
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null)
+            return;
+        ListNode mid = middle(head);
+        ListNode rev = reverse(mid);
+        ListNode prev = null;
 
+        while (head != null && rev != null) {
+            ListNode temp = head;
+            prev = head;
+            head = head.next;
+            temp.next = rev;
+            rev = rev.next;
+            temp.next.next = head;
+        }
+        if (rev != null)
+            prev.next.next = rev;
+
+    }
+
+    ListNode middle(ListNode head) {
+        ListNode slow = head, fast = head, prev = null;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            prev = slow;
+            slow = slow.next;
+        }
+        if (prev != null)
+            prev.next = null;
+        return slow;
+    }
+
+    ListNode reverse(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode ans = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return ans;
+    }
+}
