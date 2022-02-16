625142204aa38558d21b7ef22c18c87c112d6301
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2022-02-16 07:55:54 +0530

    Create 137. Single Number II.java
    137. Single Number II
    
    LeetCode Problem Solution in Java

---
+++ 137. Single Number II.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        for (int i : map.keySet()) {
+            if (map.get(i) == 1)
+                return i;
+        }
+        return 0;
+    }
+}
