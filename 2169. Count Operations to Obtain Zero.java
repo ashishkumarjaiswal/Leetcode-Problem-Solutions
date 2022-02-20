3194299802806557b4e9a4b72f4cb46a16e66562
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2022-02-20 16:30:44 +0530

    Create 2169. Count Operations to Obtain Zero.java
    2169. Count Operations to Obtain Zero
    
    LeetCode Problem Solution in Java

---
+++ 2169. Count Operations to Obtain Zero.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countOperations(int num1, int num2) {
+        int count = 0;
+        while (num1 != 0 && num2 != 0) {
+            count++;
+            if (num1 > num2) {
+                num1 -= num2;
+            } else {
+                num2 -= num1;
+            }
+        }
+        return count;
+    }
+}
