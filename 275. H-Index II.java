f8a2ab8943732844a32dcdc3fbff8eb44fb4f3cd
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2022-03-01 11:32:31 +0530

    Create 275. H-Index II.java
    275. H-Index II
    
    LeetCode Problem Solution in Java

---
+++ 275. H-Index II.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int start = 0, end = citations.length-1, n = citations.length;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (citations[mid] == n - mid)
+                return n - mid;
+            else if (citations[mid] > n - mid)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return n - start;
+    }
+}
