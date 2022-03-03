b829940f47578c6c9d5fb95d9282416ce30f712e
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2022-03-03 06:56:26 +0530

    Update 413. Arithmetic Slices.java

--- 413. Arithmetic Slices.java
+++ 413. Arithmetic Slices.java
@@ -1,3 +1,5 @@
+//Using Dp
+
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
         int[] dp = new int[nums.length];
@@ -13,5 +15,38 @@ class Solution {
             total += dp[i];
         }
         return total;
+    }
+}
+//Without Dp
+
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int ans = 0;
+
+        if (nums.length < 3)
+            return ans;
+
+        int diff = nums[1] - nums[0];
+        int count = 2;
+
+        for (int i = 2; i < nums.length; i++) {
+            while (i < nums.length && diff == nums[i] - nums[i - 1]) {
+                count++;
+                i++;
+            }
+
+            if (count >= 3) {
+                int x = 0;
+                int j = count - 2;
+                while (j > 0) {
+                    x = x + j;
+                    j--;
+                }
+                ans = ans + x;
+            }
+            count = 2;
+            diff = i < nums.length ? nums[i] - nums[i - 1] : 0;
+        }
+        return ans;
     }
 }
