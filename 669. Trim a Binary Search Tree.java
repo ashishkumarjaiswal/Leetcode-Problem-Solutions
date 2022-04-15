a1531428e1a87227ec770ce46fcd95300acd165c
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2022-04-15 08:51:05 +0530

    Create 669. Trim a Binary Search Tree.java

---
+++ 669. Trim a Binary Search Tree.java
@@ -0,0 +1,16 @@
+class Solution {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null)
+            return null;
+
+        if (root.val >= low && root.val <= high) {
+            root.left = trimBST(root.left, low, high);
+            root.right = trimBST(root.right, low, high);
+        } else if (root.val < low) {
+            root = trimBST(root.right, low, high);
+        } else if (root.val > high) {
+            root = trimBST(root.left, low, high);
+        }
+        return root;
+    }
+}
